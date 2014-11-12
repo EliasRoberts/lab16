da4eaef0e211d4c4037c983bae3acbac4a00bc62
EliasRoberts <eliasroberts99@gmail.com>
2014-11-12 14:27:33 -0600

    123456

--- 70-100pt.py
+++ 70-100pt.py
@@ -9,7 +9,7 @@
 from Tkinter import *
 root = Tk()
 drawpad = Canvas(root, width=800,height=600, background='white')
-rocket1 = drawpad.create_rectangle(400,585,405,590)
+rocket1 = drawpad.create_rectangle(400,585,405,590, fill="black")
 player = drawpad.create_oval(390,580,410,600, fill="blue")
 enemy = drawpad.create_rectangle(50,50,100,60, fill="red")
 rocket1Fired = False
@@ -46,7 +46,7 @@ class myApp(object):
         global drawpad
         global enemy
         global direction
-        global rocket
+        global rocket1
         global rocket1Fired
         x1,y1,x2,y2 = drawpad.coords(enemy)
         px1,py1,px2,py2 = drawpad.coords(player)
@@ -55,6 +55,11 @@ class myApp(object):
             direction = - 5
         elif x1 < 0:
             direction = 5
+        if rocket1Fired == True:
+            drawpad.move(rocket1,0,-5)
+        if rocket1 < 0:
+            rocket1 =  player  
+        
         drawpad.move(enemy, direction, 0)
         drawpad.after(5,self.animate)
 
@@ -73,7 +78,10 @@ class myApp(object):
         if event.char == "d":
             drawpad.move(player,4,0)
             drawpad.move(rocket1,4,0)
-    
+        if event.char == " ":
+            rocket1Fired = True
+
+        
     def collisionDetect(self, rocket):
         rx1,ry1,rx2,ry2 = drawpad.coords(rocket)
 app = myApp(root)
